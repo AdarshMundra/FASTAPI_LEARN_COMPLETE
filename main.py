795c06804259aa03ebac3912c44a57de306c9d83
Adarshmundra <adarsh.mundra1305@gmail.com>
2025-05-12 21:06:18 +0530

    basic done

--- main.py
+++ main.py
@@ -4,4 +4,20 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"data":"Blog list"}
+
+@app.get("/blog")
+def blog():
+    return {"data":"Blog list"}
+
+@app.get("/blog/unpublished")
+def blog():
+    return {"data":"Blog list"}
+
+@app.get("/blog/{id}")
+def blog(id:int):
+    return {"data":id}
+
+@app.get("/blog/{id}/comments")
+def blog(id:int):
+    return {"data":[1,2]}
